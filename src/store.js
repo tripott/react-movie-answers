be603f2187451bc07bcaf7a8c2e0ec2eed6bf9bf
Tom Wilson <tom@jackhq.com>
2017-06-27 15:58:44 -0400

    fixed

--- src/store.js
+++ src/store.js
@@ -4,7 +4,7 @@ import app from './reducers/app'
 import favorites from './reducers/favorites'
 import favorite from './reducers/favorite'
 import search from './reducers/search'
-
+import { SET_FAVORITES } from './constants'
 const store = createStore(
   combineReducers({
     app,
@@ -14,5 +14,15 @@ const store = createStore(
   }),
   applyMiddleware(thunk)
 )
+
+function listFavorites(dispatch, getState) {
+  fetch(process.env.REACT_APP_API + '/favorites')
+    .then(res => res.json())
+    .then(favorites => {
+      dispatch({ type: SET_FAVORITES, payload: favorites })
+    })
+}
+
+store.dispatch(listFavorites)
 
 export default store
